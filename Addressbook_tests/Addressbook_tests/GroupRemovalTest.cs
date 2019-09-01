ce5b022fd9492db9f62881b8ca89830d33882fd1
Сергина Светлана <Meladgi5@gmail.com>
2019-09-01 21:34:50 +0300

    Создан общий базовый класс "TaseBase" (лекция 2.1)

--- Addressbook_tests/Addressbook_tests/GroupRemovalTest.cs
+++ Addressbook_tests/Addressbook_tests/GroupRemovalTest.cs
@@ -7,21 +7,17 @@ using NUnit.Framework;
 namespace WebAddressbookTests
 {
     [TestFixture]
-    public class GroupCreationTests : TestBase
+    public class GroupRemovalTests : TestBase
     {
         [Test]
-        public void GroupCreationTest()
+        public void GroupRemovalTest()
         {
             OpenHomePage();
             Login(new AccountData("admin", "secret"));
             GoToGroupPage();
-            InitNewGroupCreation();
-            GroupData group = new GroupData("Test1");
-            group.Header = "Test2";
-            group.Footer = "Test3";
-            FillGroupForm(group);
-            SubmitGroupCreation();
+            SelectGroup(1);
+            RemoveGroup();
             ReturnToGroupsPage();
-        }                    
+        }      
     }
 }
--- Addressbook_tests/Addressbook_tests/NewContactData.cs
+++ Addressbook_tests/Addressbook_tests/NewContactData.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebAddressbookTests
 {
-    class NewContactData
+    public class NewContactData
     {
         public string firstname { get; set; }
         public string middlename { get; set; } = "";
