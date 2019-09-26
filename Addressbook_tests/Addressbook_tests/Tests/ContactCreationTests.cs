4ce3fd2fb103a76c2b55f331ecef079b41251653
Сергина Светлана <Meladgi5@gmail.com>
2019-09-26 22:01:34 +0400

    Обновление теста для создания контактов (с генератором случайных строк)

--- Addressbook_tests/Addressbook_tests/Tests/ContactCreationTests.cs
+++ Addressbook_tests/Addressbook_tests/Tests/ContactCreationTests.cs
@@ -10,19 +10,28 @@ namespace WebAddressbookTests
     [TestFixture]
     public class ContactCreationTests : AuthTestBase
     {
-        [Test]
-        public void ContactCreationTest()
+        public static IEnumerable<ContactData> RandomContactDataProvider()
         {
-            ContactData contact = new ContactData("test321");
-            contact.Lastname = "test123";
-            contact.Address = "address";
-            contact.WorkPhone = "1524-12345";
-            contact.HomePhone = "321-321";
-            contact.MobilePhone = "5555-5555";
-            contact.Email = "email@email.ru";
-            contact.Email2 = "email2@email.ru";
-            contact.Email3 = "email3@email.ru";
+            List<ContactData> contacts = new List<ContactData>();
+            for (int i = 0; i < 5; i++)
+            {
+                contacts.Add(new ContactData(GenerateRandomString(30))
+                {
+                    Lastname = GenerateRandomString(100),
+                    Address = GenerateRandomString(100),
+                    Email = GenerateRandomString(50),
+                    Email2 = GenerateRandomString(50),
+                    Email3 = GenerateRandomString(50)
+                });
+            }
 
+            return contacts;
+        }        
+
+
+        [Test, TestCaseSource("RandomContactDataProvider")]
+        public void ContactCreationTest(ContactData contact)
+        {
             List<ContactData> oldContacts = app.Contacts.GetContactList();
 
             app.Contacts.Create(contact);
