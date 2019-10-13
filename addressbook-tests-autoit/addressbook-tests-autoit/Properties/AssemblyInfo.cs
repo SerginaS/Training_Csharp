88458f73af2bba8a11b5524b414f54572f38bb5c
Сергина Светлана <Meladgi5@gmail.com>
2019-10-13 19:37:41 +0400

    Лекция 8.1. Автоматизация десктопных приложений при помощи AutoItX

---
+++ addressbook-tests-autoit/addressbook-tests-autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook-tests-autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("addressbook-tests-autoit")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b648599e-05d7-41cb-a5d2-637ad1bdaa97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
